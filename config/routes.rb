a4f4ffa03b2683eb442861d5b2e42d5a262a604c
joemaidman <joemaidman@gmail.com>
2017-04-04 17:41:18 +0100

    Adds devise gem and user migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 YelpClone::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  resources :restaurants do 
+  root 'restaurants#index'
+  resources :restaurants do
   resources :reviews
-end 
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
